7e6ab978cf94a23923cc8ebf3612969f2186fa53
oussamajafur <oussamajafur@gmail.com>
2023-03-21 15:45:16 -0700

    ‘5-sign.c’

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,10 @@
 
 /**
  * print_sign - prints the sign of a number
- *
  * @n: the number to check
- *
  * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
  */
+
 int print_sign(int n)
 {
     if (n > 0)
