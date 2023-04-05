dbb688e13bfe5e14accff0d0db42854fc9c6cc47
oussamajafur <oussamajafur@gmail.com>
2023-04-05 16:09:35 -0700

    first task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,11 +10,11 @@
    
    void _puts_recursion(char *string)
    {
-	   if(*string == '\0')
-           {
+	    if(*string == '\0')
+            {
 		       _putchar('\n');
 		       return;
-           }
-	   _putchar(*string);
-	   _puts_recursion(string + 1);
-    }  
+            }
+	    _putchar(*string);
+	    _puts_recursion(string + 1);
+   }  
