f87744dbd5755eacd3c712c39fe8755a7350c7ca
oussamajafur <oussamajafur@gmail.com>
2023-04-06 11:37:13 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,8 +11,8 @@ void _puts_recursion(char *string)
 {
 	if (*string == '\0')
         {
-		     _putchar('\n');
-		     return;
+	       _putchar('\n');
+	       return;
         }
 	_putchar(*string);
  	_puts_recursion(string + 1);
