50b02ec94c5055be9a209676437b686d32f626f7
oussamajafur <oussamajafur@gmail.com>
2023-04-06 17:35:36 -0700

    update

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,14 +2,13 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- *
  * @s: string to print
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
         {
-	       _print_rev_recursion(s + 1);
-	       _putchar(*s);
+	        _print_rev_recursion(s + 1);
+	        _putchar(*s);
 	}
 }	
