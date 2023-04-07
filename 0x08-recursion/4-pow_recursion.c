f0a20719d2cb9de178e28eaa01627912943b9b96
oussamajafur <oussamajafur@gmail.com>
2023-04-06 17:07:07 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
-  /**
-   * _pow_recursion - returns the value of c raised to the power of v.
-   * @c: base.
-   * @v: exponent.
-   * Return: value of the exponentiation.
-   */
-  int _pow_recursion(int c, int v)
-  {
-	  if (v < 0)
-		return (-1);
-	  else if (v == 0)
-		  return (1);
-	  else
-		  return (c * _pow_recursion(c, v - 1));
-  }                  		  
+/**
+ * _pow_recursion - returns the value of c raised to the power of v.
+ * @c: base.
+ * @v: exponent.
+ * Return: value of the exponentiation.
+ */
+int _pow_recursion(int c, int v)
+{
+	if (v < 0)
+                return (-1);
+	else if (v == 0)
+		return (1);
+	else
+		return (c * _pow_recursion(c, v - 1));
+}                  		  
