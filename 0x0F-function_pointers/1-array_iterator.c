5e5c41cad760dd2c7e52557d12a35ae4296669cb
oussamajafur <oussamajafur@gmail.com>
2023-04-19 15:03:43 -0700

    function given as an element

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * function parametre on each element of an array
+ *Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+
+if (action != NULL && array != NULL)
+{
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+}
+
