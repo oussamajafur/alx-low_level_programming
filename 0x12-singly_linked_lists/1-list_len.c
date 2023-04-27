3e94d9e8f64f9062a5cbaaae2683253d0001a9c4
oussamajafur <oussamajafur@gmail.com>
2023-04-27 15:20:44 -0700

    fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,11 +3,11 @@
 #include "lists.h"
 
 /**
- * list_len - 	Show the number
+ * list_len - 	Show the number of element of a list.
  *
- * @h: A link list
+ * @h: A link list.
  *
- * Return: The number
+ * Return: The number of elementin in a list.
  */
 size_t list_len(const list_t *h)
 {
