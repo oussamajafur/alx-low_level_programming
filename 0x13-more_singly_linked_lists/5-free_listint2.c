4d7d206f2d0b1149140d5cc3d9771b8418e1ffb8
oussamajafur <oussamajafur@gmail.com>
2023-05-01 10:49:44 -0700

    free a listn_t

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - linked list
+ * @head: listint_t list freed
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+
+if (head == NULL)
+return;
+
+while (*head)
+{
+temp = (*head)->next;
+
+free(*head);
+
+*head = temp;
+}
+
+*head = NULL;
+}
+
